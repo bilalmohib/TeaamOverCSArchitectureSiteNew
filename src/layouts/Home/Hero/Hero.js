fd6792500c2455374a36b8d9f24c2cb5933eda80
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-12-26 03:42:00 -0800

    Hero Completed

--- src/layouts/Home/Hero/Hero.js
+++ src/layouts/Home/Hero/Hero.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Hero.module.css";
 
 function Hero() {
   return (
     <div className={styles.hero}>
-      <h1>lorem1000</h1>
+      <div className={styles.hero_center}>
+        <p className="fs-52px white weight-5 mb-15px text-center">
+          Let’s inspire you with our work
+        </p>
+        <p className="fs-21px white weight-3 font-open-sans text-center mb-15px">
+          Click on any of the services offered by Galleria Designs to explore
+          our portfolio
+        </p>
+
+        <div className={styles.hero_cards}>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Architecture
+          </Link>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Interior Design
+          </Link>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Construction- Grey Structure
+          </Link>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Project Management
+          </Link>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Furniture
+          </Link>
+          <Link to="/" className="font-roboto fs-25px white weight-4">
+            Landscaping
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
