0a4efc802bf217c6b07caab4b99acfde8212fff7
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-12-27 10:24:47 -0800

    b

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home/Home";
 import AboutUs from "pages/AboutUs/AboutUs";
 import GetQuote from "pages/GetQuote/GetQuote";
 import Projects from "pages/Projects/Projects";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
