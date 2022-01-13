eeeca67fc17c38c9f3a0750c2c8464278980814a
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-01-13 18:28:33 +0500

    lazy-loading-added

--- src/components/Cards/ProjectCard/ProjectCard.js
+++ src/components/Cards/ProjectCard/ProjectCard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./ProjectCard.module.css";
+import LazyLoad from "react-lazyload";
 
 function ProjectCard({ img, disc, text, linkTo = "/" }) {
   return (
     <Link to={linkTo} className={styles.project_card}>
       <div className={styles.project_card_image_wrapper}>
-        <img src={img} alt="" />
+        <LazyLoad>
+          <img src={img} alt="" />
+        </LazyLoad>
         <div className={`${styles.text_wrapper} fs-20px white`}>{disc}</div>
       </div>
 
