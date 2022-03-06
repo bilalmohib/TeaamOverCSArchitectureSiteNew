183fcb5f0f41c9533046a14daef9b7787ef7395a
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-03-06 13:14:34 +0500

    Signed-off-by: Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>

---
+++ src/pages/Admin/AdminProjects/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "components/Navbar/Navbar";
+import "./style.css";
+
+const AdminProjects = () => {
+    return (
+        <div>
+            <Navbar />
+            <br /><br /><br /><br />
+            <div className="container">
+                <h1>This is the admin projects page</h1>
+            </div>
+        </div>
+    )
+}
+export default AdminProjects;
